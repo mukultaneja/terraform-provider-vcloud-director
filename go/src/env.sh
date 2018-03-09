dbdb8a3cece44626c119a715bb7ca447c9da5120
mukul taneja <mtaneja@mtaneja-a01.vmware.com>
2018-03-09 17:01:46 +0530

    Adding env.sh
    Signed-off-by: Mukul Taneja <mtaneja@mtaneja-a01.vmware.com>

--- go/src/env.sh
+++ go/src/env.sh
@@ -3,15 +3,16 @@ export TF_LOG=TRACE
 
  
 export VCD_ALLOW_UNVERIFIED_SSL=true
-#export VCD_IP="csa-sandbox.eng.vmware.com"
+# export VCD_IP="csa-sandbox.eng.vmware.com"
 
 
-#export VCD_USER="srinarayana"
-export VCD_USE_VCD_CLI_PROFILE="true"
-#
+# export VCD_USER="srinarayana"
+
+export VCD_USE_VCD_CLI_PROFILE=true
 
 #export VCD_USE_VCD_CLI_PROFILE="false"
-export VCD_IP="10.172.158.127"
+
+export VCD_IP="10.172.158.132"
 export VCD_USER="acmeadmin"
 
 export VCD_ORG= "acme"
